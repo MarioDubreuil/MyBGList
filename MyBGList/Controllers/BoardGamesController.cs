d807b7604fcdc4de9a208d817614aa8fc70d390b
Mario Dubreuil <mario_dubreuil@hotmail.com>
2022-10-14 21:37:30 -0400

    Add board games controller

---
+++ MyBGList/Controllers/BoardGamesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBGList.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BoardGamesController : ControllerBase
+    {
+        private readonly ILogger<BoardGamesController> _logger;
+
+        public BoardGamesController(ILogger<BoardGamesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<BoardGame> Get()
+        {
+            return new[]
+            {
+                new BoardGame()
+                {
+                    Id = 1,
+                    Name = "Axies & Allies",
+                    Year = 1981
+                },
+                new BoardGame()
+                {
+                    Id = 2,
+                    Name = "Citadels",
+                    Year = 2000
+                },
+                new BoardGame()
+                {
+                    Id = 3,
+                    Name = "Terraforming Mars",
+                    Year = 2016
+                }
+            };
+    }
+}
+}
