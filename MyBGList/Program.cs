b077c9bcf60da4af81996fc561a391e68fa005eb
Mario Dubreuil <mario_dubreuil@hotmail.com>
2022-10-15 22:19:26 -0400

    Add Cors

--- MyBGList/Program.cs
+++ MyBGList/Program.cs
@@ -1,3 +1,6 @@
+using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Cors;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(cfg =>
+    {
+        cfg.WithOrigins(builder.Configuration["AllowedOrigins"]);
+        cfg.AllowAnyHeader();
+        cfg.AllowAnyMethod();
+    });
+    options.AddPolicy(name: "AnyOrigin", cfg =>
+    {
+        cfg.AllowAnyOrigin();
+        cfg.AllowAnyHeader();
+        cfg.AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -27,11 +46,13 @@ else
 
 app.UseHttpsRedirection();
 
+app.UseCors();
+
 app.UseAuthorization();
 
-app.MapGet("/error", () => Results.Problem());
+app.MapGet("/error", [EnableCors("AnyOrigin")] () => Results.Problem());
 
-app.MapGet("/error/test", () => { throw new Exception("test"); });
+app.MapGet("/error/test", [EnableCors("AnyOrigin")] () => { throw new Exception("test"); });
 
 app.MapControllers();
 
