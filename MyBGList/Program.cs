17b920650473d1a2ae365f684c45089bc5d21ea3
Mario Dubreuil <mario_dubreuil@hotmail.com>
2022-11-16 19:16:06 -0500

    Customize a model binding error

--- MyBGList/Program.cs
+++ MyBGList/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options => options.ModelBindingMessageProvider.SetAttemptedValueIsInvalidAccessor((x, y) => $"***** The value '{x}' is not valid for {y}. *****"));
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
